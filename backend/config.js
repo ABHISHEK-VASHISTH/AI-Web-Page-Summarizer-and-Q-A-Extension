cedf8b97fe99cdcb9a2dc6a83b5d0fb28f455c1d
Abhishek Vashisth <ap9359@srmist.edu.in>
2026-04-25 14:34:42 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+
+const PAGEMIND_CONFIG = {
+  GEMINI_API_KEY: "Paste_Your_key_here",
+  GROQ_API_KEY: "Paste_Your_key_here",
+  OPENROUTER_API_KEY: "Paste_Your_key_here",
+  HUGGINGFACE_API_KEY: "Paste_Your_key_here",
+  PREFERRED_PROVIDER: "auto"
+};
