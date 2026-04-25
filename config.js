349c2a871cd7181423dc8fe27d3061aad607c0e0
Abhishek Vashisth <ap9359@srmist.edu.in>
2026-04-25 14:33:42 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-
-const PAGEMIND_CONFIG = {
-  GEMINI_API_KEY: "Paste_Your_key_here",
-  GROQ_API_KEY: "Paste_Your_key_here",
-  OPENROUTER_API_KEY: "Paste_Your_key_here",
-  HUGGINGFACE_API_KEY: "Paste_Your_key_here",
-  PREFERRED_PROVIDER: "auto"
-};
-
